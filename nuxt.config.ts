939f7701240a68293f4f19ef4a1f32ab681adb47
omuraisu <orehasiawaseninaritai@gmail.com>
2026-08-23 16:10:36 +0900

    PrimeVueを導入し、Auraテーマを設定

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,15 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
+import Aura from '@primeuix/themes/aura'
+
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  modules: ['@primevue/nuxt-module'],
+  primevue: {
+    options: {
+      theme: {
+        preset: Aura
+      }
+    }
+  },
+  css: ['primeicons/primeicons.css']
 })
