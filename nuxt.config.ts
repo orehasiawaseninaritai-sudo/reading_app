629dedc61b5178ce067d94356815f894ad8789cf
omuraisu <orehasiawaseninaritai@gmail.com>
2026-08-29 16:35:36 +0900

    MySQLへのDB接続設定とbooksテーブル作成

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,14 @@
 import Aura from '@primeuix/themes/aura'
 
 export default defineNuxtConfig({
+  runtimeConfig: {
+    dbHost: process.env.DB_HOST,
+    dbPort: process.env.DB_PORT,
+    dbUser: process.env.DB_USER,
+    dbPassword: process.env.DB_PASSWORD,
+    dbName: process.env.DB_NAME
+  },
+
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
   modules: ['@primevue/nuxt-module'],
---
+++ server/utils/db.ts
@@ -0,0 +1,11 @@
+import mysql from 'mysql2/promise';
+
+const config = useRuntimeConfig();
+
+export const pool = mysql.createPool({
+    host: config.dbHost,
+    port: config.dbPort,
+    user: config.dbUser,
+    password: config.dbPassword,
+    database: config.dbName
+});
